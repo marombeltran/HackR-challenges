097dede9b26ec16a8df9cb9132dc4dcb345cba51
Marom Beltran <mrmbeltran@proton.me>
2024-06-29 11:42:10 -0600

    prepare

---
+++ Prepare/Linux Shell/A Personalized Echo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Write a Bash script which accepts  as input and displays the greeting "Welcome (name)"
+
+read name
+echo "Welcome $name"
---
+++ Prepare/Linux Shell/Looping with Numbers.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Use a for loop to display the natural numbers from  to .
+for i in {1..50}; do
+    echo $i
+done
